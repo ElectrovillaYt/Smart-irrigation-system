40a4413941dce31f3e18aa4d9fb16795944f1854
ElectrovillaYt <electrodethe061@gmail.com>
2023-05-08 14:03:24 +0530

    Arduino code

---
+++ smart_irrigation_3.0.ino
@@ -0,0 +1,99 @@
+
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define sensor A0
+#define manual_switch 2
+#define automode 3
+#define dry_sense 4
+#define Out 5
+
+void setup() {
+  pinMode(sensor, INPUT);
+  pinMode(manual_switch, INPUT_PULLUP);
+  pinMode(automode, INPUT_PULLUP);
+  pinMode(dry_sense, INPUT);
+  pinMode(Out, OUTPUT);
+
+  lcd.begin();
+  lcd.backlight();
+
+  lcd.setCursor(1, 0);
+  lcd.print("System Loading");
+  for (int a = 0; a <= 15; a++) {
+    lcd.setCursor(a, 1);
+    lcd.print(".");
+    delay(150);
+  }
+  lcd.clear();
+}
+
+int M_value;
+void readmoisture() {
+  M_value = analogRead(sensor);
+  M_value = map(M_value, 0, 1024, 100, -1);
+  lcd.setCursor(0, 0);
+  lcd.print("Moisture: ");
+  lcd.print(M_value);
+  lcd.print("%");
+  lcd.print(" ");
+}
+
+int state = 0;
+void maunalswitch() {
+  int in = digitalRead(manual_switch);
+  if (in == 0) {
+    state = !state;
+    digitalWrite(Out, state);
+    delay(200);
+  }
+  if (state == 1) {
+    lcd.setCursor(8, 1);
+    lcd.print("PUMP ON");
+    lcd.print(" ");
+  } else {
+    lcd.setCursor(8, 1);
+    lcd.print("PUMP OFF");
+    lcd.print(" ");
+  }
+}
+
+
+int auto_state = 0;
+void automode_switch() {
+  int value = digitalRead(automode);
+  if (value == 0) {
+    auto_state = !auto_state;
+    delay(200);
+  }
+  if (auto_state == 1) {
+    lcd.setCursor(0, 1);
+    lcd.print("Auto:");
+    lcd.print("  ");
+
+    if (M_value <= 20) {
+      state = 1;
+      digitalWrite(Out, 1);
+    } else {
+      state = 0;
+      digitalWrite(Out, 0);
+    }
+  } else {
+    lcd.setCursor(0, 1);
+    lcd.print("Manual:");
+    lcd.print(" ");
+  }
+}
+
+
+void display() {
+  readmoisture();
+  maunalswitch();
+  automode_switch();
+}
+
+
+void loop() {
+  display();
+}
